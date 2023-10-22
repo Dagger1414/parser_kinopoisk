6ccc028791241940a4bbe716e55dde372a523a1e
Serj Vavilin <dagger141414@gmail.com>
2023-10-22 22:30:25 +0300

    added req_to_apy class, base config

---
+++ settings/config.example.py
@@ -0,0 +1,7 @@
+"""Модуль с настройками доступа к API Kinoposk.dev"""
+
+
+class Settings:
+    """ Класс настроек"""
+    api_url: str = "https://api.kinopoisk.dev"
+    api_key: str = "Ключ к API, полученный через телеграм бот @kinopoiskdev_bot"
